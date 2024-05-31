f651e0bbc236c75e63873634a6f730b6a9479a3d
GET Academy <terje@kolderup.net>
2024-05-31 12:27:28 +0200

    Initial commit

---
+++ MinesweeperUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MinesweeperUI
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+            app.Run(window);
+        }
+    }
+}
